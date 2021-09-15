7611f741f7f350d9a9469f8babfdd8ea93673c29
tjudice <tjudice@umich.edu>
2021-09-14 22:48:46 -0400

    Adding swiftChatter files

---
+++ lab0/swiftChatter/swiftChatter/swiftChatter/ChatTableCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ChattTableCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+}
